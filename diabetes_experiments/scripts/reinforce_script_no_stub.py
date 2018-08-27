b4b0a16f76f6d2b0f7f027f0e94a48841bcbc697
Jonas Nordhaug Myhre <jonas@dhcp775-ans.wifi.uit.no>
2018-08-27 14:56:11 +0200

    plotting for reinforce no stub

--- diabetes_experiments/scripts/reinforce_script_no_stub.py
+++ diabetes_experiments/scripts/reinforce_script_no_stub.py
@@ -4,6 +4,12 @@ from rllab.envs.gym_env import GymEnv
 from rllab.envs.normalized_env import normalize
 from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
 from rllab.misc.instrument import run_experiment_lite
+import sys
+# sys.path.insert(0, '/home/jonas/Documents/git/EXTERNAL/rllab_fork/diabetes_experiments/')
+sys.path.insert(0, '/Users/jonas/Documents/git/rllab/diabetes_experiments/')
+from load_and_sim_policy import render_and_plot_policy
+
+import os.path as osp
 
 try:
     import seaborn as sns
@@ -11,26 +17,26 @@ try:
 except ImportError:
     print('\nConsider installing seaborn (pip install seaborn) for better plotting!')
 
-
 # ==========================================================================
 # OpenAI diabetes envs - HovorkaInterval starts at the same value every time,
 # HovorkaIntervalRandom starts at a random value
 # ==========================================================================
 
 def run_task(*_):
+
+    # ===================
+    # Defining the policy
+    # ===================
     env = normalize(GymEnv('HovorkaInterval-v0'))
     # env.wrapped_env.env.env.env.reward_flag = 'absolute'
     env.wrapped_env.env.env.reward_flag = 'absolute'
-
-
-    baseline = LinearFeatureBaseline(env_spec=env.spec)
+    ## Setting the neural network architecture
+    # hidden_sizes=(8,)
+    # hidden_sizes=(32, 32)
+    # hidden_sizes=(100, 50, 25)
 
     learn_std = True
-    init_std=2
-
-    # hidden_sizes=(8,)
-    hidden_sizes=(32, 32)
-    # hidden_sizes=(100, 50, 25)
+    init_std = 1
 
     policy = GaussianMLPPolicy(
         env_spec=env.spec,
@@ -42,16 +48,20 @@ def run_task(*_):
     # =======================
     # Defining the algorithm
     # =======================
+    baseline = LinearFeatureBaseline(env_spec=env.spec)
+
     batch_size = 5000
     n_itr = 3
-    gamma = .9
+    gamma = .99
     step_size = 0.01
+    max_path_length = 96
 
     algo = VPG(
         env=env,
         policy=policy,
         baseline=baseline,
         batch_size=batch_size,
+        max_path_length=max_path_length,
         n_itr=n_itr,
         discount=gamma,
         step_size=step_size
@@ -60,9 +70,17 @@ def run_task(*_):
 
     return algo
 
+# hidden_arc = [str(i) for i in hidden_sizes]
+# hidden_arc = '_'.join(hidden_arc)i
 
-# log_dir = '~/Dropbox/results/jonas_experiments/no_stub/'
-log_dir = './'
+# data_dir = 'Reinforce_batchSize_{}_nIters_{}_stepSize_{}_gamma_{}_initStd_{}{}_policyPar_{}_reward_{}'\
+        # .format(batch_size, n_itr, step_size,''.join(str(gamma).split('.')), init_std, learn_std, hidden_arc, reward_fun)
+
+data_dir = 'VPG_default'
+PROJECT_PATH = '/Users/jonas/Dropbox/results/miguel_experiments/'
+log_dir = PROJECT_PATH + data_dir
+log_dir='./'
+
 # Running and saving the experiment
 run_experiment_lite(
     run_task,
@@ -76,7 +94,15 @@ run_experiment_lite(
     # will be used
     # exp_prefix="Reinforce_" + env_name,
     # exp_prefix=data_dir
-    plot=False
+    seed=1,
+    mode="local",
+    plot=False,
+    use_cloudpickle=False,
+    added_project_directories=[osp.abspath(osp.join(osp.dirname(__file__), '.'))]
 )
 
+## Testing the policy
+filename = log_dir + '/params.pkl'
+figure_filename = data_dir + '.png'
 
+#render_and_plot_policy(filename, figure_filename)
