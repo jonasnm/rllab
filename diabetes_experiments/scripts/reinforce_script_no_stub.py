c5f75f1b1b043ae55b52e39bff53d8fdcac846d7
Jonas Nordhaug Myhre <jonas@dhcp775-ans.wifi.uit.no>
2018-08-28 11:38:32 +0200

    reinforce_script + plotting

--- diabetes_experiments/scripts/reinforce_script_no_stub.py
+++ diabetes_experiments/scripts/reinforce_script_no_stub.py
@@ -9,7 +9,8 @@ import sys
 sys.path.insert(0, '/Users/jonas/Documents/git/rllab/diabetes_experiments/')
 from load_and_sim_policy import render_and_plot_policy
 
-import os.path as osp
+# File name for saving
+RL = 'VPG'
 
 try:
     import seaborn as sns
@@ -17,92 +18,84 @@ try:
 except ImportError:
     print('\nConsider installing seaborn (pip install seaborn) for better plotting!')
 
-# ==========================================================================
-# OpenAI diabetes envs - HovorkaInterval starts at the same value every time,
-# HovorkaIntervalRandom starts at a random value
-# ==========================================================================
+reward_functions = ('absolute', 'gaussian', 'gaussian_with_insulin' )
+NN_sizes = ((8,), (32, 32), (100, 50, 25))
 
-def run_task(*_):
+for k in range(len(reward_functions)):
+    for i in range(len(NN_sizes)):
 
-    # ===================
-    # Defining the policy
-    # ===================
-    env = normalize(GymEnv('HovorkaInterval-v0'))
-    # env.wrapped_env.env.env.env.reward_flag = 'absolute'
-    env.wrapped_env.env.env.reward_flag = 'absolute'
-    ## Setting the neural network architecture
-    # hidden_sizes=(8,)
-    # hidden_sizes=(32, 32)
-    # hidden_sizes=(100, 50, 25)
+        # ==========================================================================
+        # OpenAI diabetes envs - HovorkaInterval starts at the same value every time,
+        # HovorkaIntervalRandom starts at a random value
+        # ==========================================================================
 
-    learn_std = True
-    init_std = 1
+        def run_task(*_):
+            env = normalize(GymEnv('HovorkaInterval-v0'))
+            # env.wrapped_env.env.env.env.reward_flag = 'absolute'
+            env.wrapped_env.env.env.reward_flag = reward_functions[k]
 
-    policy = GaussianMLPPolicy(
-        env_spec=env.spec,
-        hidden_sizes=hidden_sizes,
-        learn_std=learn_std,
-        init_std=init_std
-    )
+            baseline = LinearFeatureBaseline(env_spec=env.spec)
 
-    # =======================
-    # Defining the algorithm
-    # =======================
-    baseline = LinearFeatureBaseline(env_spec=env.spec)
+            learn_std = True
+            init_std = 1
 
-    batch_size = 5000
-    n_itr = 3
-    gamma = .99
-    step_size = 0.01
-    max_path_length = 96
+            hidden_sizes = NN_sizes[i]
+            # hidden_sizes=(8,)
+            # hidden_sizes=(32, 32)
+            # hidden_sizes=(100, 50, 25)
 
-    algo = VPG(
-        env=env,
-        policy=policy,
-        baseline=baseline,
-        batch_size=batch_size,
-        max_path_length=max_path_length,
-        n_itr=n_itr,
-        discount=gamma,
-        step_size=step_size
-    )
-    algo.train()
+            policy = GaussianMLPPolicy(
+                env_spec=env.spec,
+                hidden_sizes=hidden_sizes,
+                learn_std=learn_std,
+                init_std=init_std
+            )
 
-    return algo
+            # =======================
+            # Defining the algorithm
+            # =======================
+            batch_size = 5000
+            n_itr = 200
+            gamma = .99
+            step_size = 0.01
+            # max_path_length = 96,
 
-# hidden_arc = [str(i) for i in hidden_sizes]
-# hidden_arc = '_'.join(hidden_arc)i
+            algo = VPG(
+                env=env,
+                policy=policy,
+                baseline=baseline,
+                batch_size=batch_size,
+                # max_path_length=max_path_length,
+                n_itr=n_itr,
+                discount=gamma,
+                step_size=step_size
+            )
+            algo.train()
 
-# data_dir = 'Reinforce_batchSize_{}_nIters_{}_stepSize_{}_gamma_{}_initStd_{}{}_policyPar_{}_reward_{}'\
-        # .format(batch_size, n_itr, step_size,''.join(str(gamma).split('.')), init_std, learn_std, hidden_arc, reward_fun)
+        NN_folder = [str(j) for j in NN_sizes[i]]
+        NN_folder = '_'.join(NN_folder)
 
-data_dir = 'VPG_default'
-PROJECT_PATH = '/Users/jonas/Dropbox/results/miguel_experiments/'
-log_dir = PROJECT_PATH + data_dir
-log_dir='./'
+        log_dir = '/Users/jonas/Dropbox/results/miguel_experiments/' + RL + reward_functions[k] + algo.batch_size + NN_folder
+        # log_dir = './'
+        # Running and saving the experiment
+        run_experiment_lite(
+            run_task,
+            # algo.train(),
+            log_dir=log_dir,
+            # n_parallel=2,
+            n_parallel=1,
+            # Only keep the snapshot parameters for the last iteration
+            snapshot_mode="last",
+            # Specifies the seed for the experiment. If this is not provided, a random seed
+            # will be used
+            # exp_prefix="Reinforce_" + env_name,
+            # exp_prefix=data_dir
+            plot=False
+        )
 
-# Running and saving the experiment
-run_experiment_lite(
-    run_task,
-    # algo.train(),
-    log_dir=log_dir,
-    # n_parallel=2,
-    n_parallel=1,
-    # Only keep the snapshot parameters for the last iteration
-    snapshot_mode="last",
-    # Specifies the seed for the experiment. If this is not provided, a random seed
-    # will be used
-    # exp_prefix="Reinforce_" + env_name,
-    # exp_prefix=data_dir
-    seed=1,
-    mode="local",
-    plot=False,
-    use_cloudpickle=False,
-    added_project_directories=[osp.abspath(osp.join(osp.dirname(__file__), '.'))]
-)
+        ## Testing the policy
+        data_dir = '/Users/jonas/Dropbox/results/miguel_experiments/' + RL + reward_functions[k] + algo.batch_size + NN_folder + RL + '_default'
+        filename = log_dir + '/params.pkl'
+        figure_filename = data_dir + '.png'
 
-## Testing the policy
-filename = log_dir + '/params.pkl'
-figure_filename = data_dir + '.png'
-
-#render_and_plot_policy(filename, figure_filename)
+        render_and_plot_policy(filename, figure_filename)
