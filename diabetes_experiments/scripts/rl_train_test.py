f29b5a9709d9fed32ac4b8cae6f0fa57dd68598d
Jonas Nordhaug Myhre <jonas@dhcp35-ans2.wifi.uit.no>
2018-09-20 12:06:19 +0200

    rl script cambridge

--- diabetes_experiments/scripts/rl_train_test.py
+++ diabetes_experiments/scripts/rl_train_test.py
@@ -1,4 +1,4 @@
-from rllab.algos.vpg import VPG
+from rllab.algos.trpo import TRPO
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
 from rllab.envs.gym_env import GymEnv
 from rllab.envs.normalized_env import normalize
@@ -10,7 +10,7 @@ sys.path.insert(0, '/Users/jonas/Documents/git/rllab/diabetes_experiments/')
 from load_and_sim_policy import render_and_plot_policy
 
 # File name for saving
-RL = 'VPG'
+RL = 'TRPO'
 
 try:
     import seaborn as sns
@@ -18,13 +18,10 @@ try:
 except ImportError:
     print('\nConsider installing seaborn (pip install seaborn) for better plotting!')
 
-# models = ('HovorkaAbsolute-v0', 'HovorkaBinary-v0', 'HovorkaGaussian-v0', 'HovorkaGaussianInsulin-v0', 'HovorkaHovorka-v0')
-# models = ('HovorkaAbsolute-v0', 'HovorkaBinary-v0', 'HovorkaGaussian-v0', 'HovorkaGaussianInsulin-v0')
-# models = ('HovorkaRandomAbsolute-v0', 'HovorkaRandomBinary-v0', 'HovorkaRandomGaussian-v0', 'HovorkaRandomGaussianInsulin-v0')
-models = ('HovorkaMealsAbsolute-v0', 'HovorkaMealsBinary-v0', 'HovorkaMealsGaussian-v0', 'HovorkaMealsGaussianInsulin-v0')
-# models = ('HovorkaMealsGaussian-v0', 'HovorkaMealsGaussianInsulin-v0')
+# models = ('HovorkaMealsGaussian-v0', 'HovorkaMealsAbsolute-v0', 'HovorkaMealsBinary-v0', 'HovorkaMealsGaussianInsulin-v0')
+models = ('CambridgeMealsGaussian-v0', 'CambridgeMealsAbsolute-v0', 'CambridgeMealsBinary-v0', 'CambridgeMealsGaussianInsulin-v0')
 # NN_sizes = ((8,), (32, 32), (100, 50, 25))
-NN_sizes = ((32, 32), (100, 50, 25))
+NN_sizes = ((100, 50, 25), (32, 32))
 
 for k in range(len(models)):
     for i in range(len(NN_sizes)):
@@ -63,7 +60,7 @@ for k in range(len(models)):
             step_size = 0.01
             # max_path_length = 96,
 
-            algo = VPG(
+            algo = TRPO(
                 env=env,
                 policy=policy,
                 baseline=baseline,
@@ -97,7 +94,7 @@ for k in range(len(models)):
         )
 
         ## Testing the policy
-        data_dir = '/Users/jonas/Dropbox/results/miguel_experiments/' + RL + '/' + models[k] + '/' + '5000' + '/' + NN_folder + '/' + RL + '_default'
+        data_dir = '/Users/jonas/Dropbox/results/miguel_experiments/cambridge/' + RL + '/' + models[k] + '/' + '5000' + '/' + NN_folder + '/' + RL + '_default'
         filename = log_dir + '/params.pkl'
         figure_filename = data_dir + '.png'
         title = RL + '_' + models[k] + '_' + NN_folder
